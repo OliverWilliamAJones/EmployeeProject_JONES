79db0b81381ae4bd0077bf70f7d3d271c966859c
Revy <25101381@usc.edu.ph>
2026-09-14 15:52:24 +0800

    Add employee classes

---
+++ src/BasePlusCommissionEmployee.java
@@ -0,0 +1,116 @@
+package version1;
+
+public class BasePlusCommissionEmployee {
+
+    private int empID;
+    private String empName;
+    private double totalSale;
+    private double baseSalary;
+
+    public BasePlusCommissionEmployee() {
+        empID = 0;
+        empName = "N/A";
+        totalSale = 0;
+        baseSalary = 0;
+    }
+
+    public BasePlusCommissionEmployee(int empID, String empName) {
+        this.empID = empID;
+        this.empName = empName;
+        totalSale = 0;
+        baseSalary = 0;
+    }
+
+    public BasePlusCommissionEmployee(int empID, String empName, double totalSale, double baseSalary) {
+        this.empID = empID;
+        this.empName = empName;
+        setTotalSale(totalSale);
+        setBaseSalary(baseSalary);
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public double getTotalSale() {
+        return totalSale;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public void setTotalSale(double totalSale) {
+        if (totalSale >= 0) {
+            this.totalSale = totalSale;
+        } else {
+            this.totalSale = 0;
+        }
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        if (baseSalary >= 0) {
+            this.baseSalary = baseSalary;
+        } else {
+            this.baseSalary = 0;
+        }
+    }
+
+    public double computeSalary() {
+
+        double commissionRate;
+
+        if (totalSale < 50000) {
+            commissionRate = 0.05;
+        } else if (totalSale < 100000) {
+            commissionRate = 0.10;
+        } else if (totalSale < 500000) {
+            commissionRate = 0.15;
+        } else {
+            commissionRate = 0.20;
+        }
+
+        return baseSalary + (totalSale * commissionRate);
+    }
+
+    public void displayBasePlusCommissionEmployee() {
+        System.out.printf(
+                "ID: %d | Name: %s | Total Sale: ₱%,.2f | Base Salary: ₱%,.2f%n",
+                empID, empName, totalSale, baseSalary
+        );
+    }
+
+    private double getCommissionRate() {
+
+        if (totalSale < 50000) {
+            return 0.05;
+        } else if (totalSale < 100000) {
+            return 0.10;
+        } else if (totalSale < 500000) {
+            return 0.15;
+        } else {
+            return 0.20;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "BasePlusCommissionEmployee [ID: %d, Name: %s, Sales: ₱%,.2f, Base Salary: ₱%,.2f, Commission Rate: %.0f%%, Total Salary: ₱%,.2f]",
+                empID, empName, totalSale, baseSalary,
+                getCommissionRate() * 100, computeSalary()
+        );
+    }
+}
